eba6763733845bc326b16326037c92d1534af267
mir6u <discordbackupacc1234@gmail.com>
2023-11-02 16:47:54 +0300

    metadata img

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,23 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import logo from './logo.webp'
+import logo from "./logo.webp";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Mirui's Portfolio",
   description: "My Personal Portfolio",
+  keywords: ["Mirui", "NextJS", "React", "Portfolio", "Frontend"],
+  openGraph: {
+    images: [
+      {
+        url: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/220px-Image_created_with_a_mobile_phone.png",
+        width: 200,
+        height: 200,
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
@@ -19,6 +29,12 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="shortcut icon" href="logo.ico" type="image/x-icon" />
+        <meta
+          content="https://embed.com/this-is-the-site-url"
+          property="og:url"
+        />
+        <meta content="https://embed.com/embedimage.png" property="og:image" />
+        <meta content="#43B581" data-react-helmet="true" name="theme-color" />
       </head>
       <body className={inter.className}>{children}</body>
     </html>
